5c08046b600f20cadc8b5d2d7ea53bdaae32d09c
blacksnow233 <longyuhuhao@126.com>
2018-12-28 14:44:48 +0800

    成功使用vue

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 module.exports = {
   entry: {
     bundle: path.resolve(__dirname, '../src/main.js')
@@ -11,16 +12,52 @@ module.exports = {
   },
   module: {
     rules: [
-      
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.(png|jpg|jfif|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1000,
+              name: 'static/images/[hash:8].[name].[ext]'
+            }
+          },
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'url-loader',
+          }
+        ]
+      },
+
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'index.html')
-    })
+    }),
+    new VueLoaderPlugin(),
   ],
   resolve: {
-    
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': path.resolve(__dirname, '../src')
+    },
+    extensions: ['*', '.js', '.json', '.vue']
   }
 };
 
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+import Vue from 'vue'
+import App from './App'
+
+new Vue({
+  el: '#app',
+  components: {App},
+  template: '<App/>'
+})
